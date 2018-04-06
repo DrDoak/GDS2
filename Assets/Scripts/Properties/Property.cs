fdff74e793233fa361f4523c3d65c25ec079dd6f
Vikram <vikr4m.kohli@gmail.com>
2018-04-06 18:07:46 -0500

    Added property superclass

---
+++ Assets/Scripts/Properties/Property.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Property : MonoBehaviour
+{
+
+    protected abstract void OnCreation();
+    protected abstract void OnHit();
+    protected abstract void OnHitConfirm();
+    protected abstract void OnDeath();
+    protected abstract void OnUpdate();
+    protected abstract void OnCollision();
+    protected abstract void OnAttack();
+
+}
