b458e4912537b4ab6e80d90338eed84e60bd7e97
Vikram <vikr4m.kohli@gmail.com>
2018-04-08 17:54:17 -0500

    Updated explosive

--- Assets/Scripts/Properties/PR_Explosive.cs
+++ Assets/Scripts/Properties/PR_Explosive.cs
@@ -6,10 +6,10 @@ public class PR_Explosive : Property {
 
     Vector2 scl = new Vector2(5.0f, 5.0f);
     Vector2 off = new Vector2(0f, 0f);
-    float dmg = 25.0f;
+    float dmg = 50.0f;
     float stun = 1.0f;
-    float hd = 1.0f;
-    Vector2 kb = new Vector2(3.0f, 3.0f);
+    float hd = 0.5f;
+    Vector2 kb = new Vector2(25.0f, 60.0f);
 
     public override void OnCreation()
     {
@@ -19,7 +19,7 @@ public class PR_Explosive : Property {
     public override void OnHit()
     {
         Debug.Log("PR_Explosive on hit (kaboom)");
-        GetComponent<HitboxMaker>().CreateHitbox(scl, off, dmg, stun, hd, kb);
+        GetComponent<HitboxMaker>().CreateHitbox(scl, off, dmg, stun, hd, kb, false);
     }
 
 }
