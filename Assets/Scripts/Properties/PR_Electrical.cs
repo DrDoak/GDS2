89d70d81fc279e12acd5239b2371157743c3a82a
DrDoak <larryzhao740@yahoo.com>
2018-04-16 15:05:00 -0500

    Changed some properties to make more sense

--- Assets/Scripts/Properties/PR_Electrical.cs
+++ Assets/Scripts/Properties/PR_Electrical.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class PR_Electrical : Property {
 
+	GameObject fx;
+
 	public override void OnHitboxCreate (Hitbox hitboxCreated) {
 		hitboxCreated.Element = ElementType.LIGHTNING;
 		hitboxCreated.Damage = hitboxCreated.Damage * 1.2f;
+	}
+	public override void OnAddProperty()
+	{
+		fx = GetComponent<PropertyHolder> ().AddBodyEffect (FXBody.Instance.FXLightning);
+		//   GetComponent<PhysicsSS>().SetGravityScale(-2.0f);
+	}
+
+	public override void OnRemoveProperty()
+	{
+		GetComponent<PropertyHolder> ().RemoveBodyEffect (fx);
+		//GetComponent<PhysicsSS>().SetGravityScale(-1.0f);
 	}
 }
