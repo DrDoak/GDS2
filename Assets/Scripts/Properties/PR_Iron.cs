1838547187da6b77e227590cfffac9259cdcda52
Vikram <vikr4m.kohli@gmail.com>
2018-04-15 20:56:10 -0500

    Iron property

---
+++ Assets/Scripts/Properties/PR_Iron.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PR_Iron : Property {
+
+    Resistence physResist;
+    Resistence fireResist;
+    Resistence lightningResist;
+
+    public override void OnAddProperty()
+    {
+        physResist = GetComponent<Attackable>().AddResistence(ElementType.PHYSICAL, 25.0f, false, false, 0.0f, 70.0f, 70.0f);
+        fireResist = GetComponent<Attackable>().AddResistence(ElementType.FIRE, 50.0f, false, false);
+        lightningResist = GetComponent<Attackable>().AddResistence(ElementType.LIGHTNING, -25.0f, false, false);
+        GetComponent<BasicMovement>().SetMoveSpeed(GetComponent<BasicMovement>().MoveSpeed / 2.0f);
+        GetComponent<BasicMovement>().SetJumpData(GetComponent<BasicMovement>().JumpHeight / 2.0f, GetComponent<BasicMovement>().TimeToJumpApex);
+        GetComponent<PhysicsSS>().SetGravityScale(-2.0f);
+    }
+
+    public override void OnRemoveProperty()
+    {
+        GetComponent<Attackable>().RemoveResistence(physResist);
+        GetComponent<Attackable>().RemoveResistence(fireResist);
+        GetComponent<Attackable>().RemoveResistence(lightningResist);
+        GetComponent<BasicMovement>().SetMoveSpeed(GetComponent<BasicMovement>().MoveSpeed * 2.0f);
+        GetComponent<BasicMovement>().SetJumpData(GetComponent<BasicMovement>().JumpHeight * 2.0f, GetComponent<BasicMovement>().TimeToJumpApex);
+        GetComponent<PhysicsSS>().SetGravityScale(-1.0f);
+    }
+
+}
