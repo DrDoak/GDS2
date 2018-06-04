5b666e4f7f81295d1ff74da7d377d176f42aaf71
DrDoak <larryzhao740@yahoo.com>
2018-06-04 16:33:59 -0500

    Connected Cathedral to Palace

---
+++ Assets/Scripts/Characters/DropItemDeath.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropItemDeath : MonoBehaviour {
+	public GameObject ItemDropped;
+	// Use this for initialization
+	void Update() {
+		if (ItemDropped != null && !GetComponent<Attackable>().Alive) {
+			Instantiate (ItemDropped, transform.position, Quaternion.identity);
+			ItemDropped = null;
+		}
+	}
+}
